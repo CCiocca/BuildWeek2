339a844a28a1a10502feee611c698d69f2d293f5
FedericoMottola <fedemottola@live.it>
2023-12-19 16:53:52 +0100

    upd-searchpage-nav

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,4 +1,11 @@
 const myUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
+let arrayAlbums = [];
+
+window.onload = () => {
+  getData("alt-j");
+  getData("queen");
+  getData("dua-lipa");
+};
 
 // recuperare l'elemento che compie l'azione
 const searchBTN = document.getElementById("searchBtn");
@@ -18,10 +25,56 @@ function loadSong(query) {
     .catch((error) => console.error("Errore:", error));
 }
 
+function getData(query) {
+  fetch(`${myUrl}[${query}]`, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data)
+      handleCreateArrays(data);
+      handlePrintData();
+    })
+    .catch((error) => console.error("Errore durante la fetch:", error));
+}
+
+function handleCreateArrays(data) {
+  createArrayAlbums(data);
+  localStorage.setItem("arrayAlbums", JSON.stringify(arrayAlbums));
+}
+function createArrayAlbums(data) {
+  for (let i = 0; i < data.data.length; i++) {
+    let album = data.data[i].album;
+    const isExistingAlbum = arrayAlbums.some(
+      (alb) => alb.id.toString() === album.id.toString()
+    );
+    if (!isExistingAlbum) arrayAlbums.push(album);
+  }
+}
+
 function searchSong() {
   const input = document.getElementById("inputSearch");
   query = input.value;
   return query;
+}
+
+function handlePrintData() {
+  printSideList();
+}
+
+
+// print side list
+function printSideList() {
+  const containerSideList = document.getElementById("containerSideList");
+  containerSideList.innerHTML = "";
+  for (let i = 0; i < arrayAlbums.length; i++) {
+    const newItem = document.createElement("li");
+    newItem.classList.add("my-2");
+
+    newItem.innerText = `${arrayAlbums[i].title}`;
+
+    containerSideList.appendChild(newItem);
+  }
 }
 
 function loadResults(data) {
@@ -66,6 +119,35 @@ function nascondiColonna() {
 
   const colCentral = document.querySelector(".col-8");
   colCentral.classList.add("col-10");
+}
+document.addEventListener("DOMContentLoaded", function () {
+  let rightColumn = document.getElementById("mainRightOpened");
+  let centerColumn = document.querySelector(".col-8");
+
+  let peopleIcon = document.getElementById("peopleIcon");
+
+  function mostraColonnaDestra() {
+    // Mostra la colonna destra
+    rightColumn.style.display = "block";
+
+    // Ripristina le dimensioni della colonna centrale
+    centerColumn.classList.remove("col-10");
+    centerColumn.classList.add("col-8");
+  }
+
+  // Aggiungi un gestore di eventi per il click sull'icona delle persone
+  peopleIcon.addEventListener("click", mostraColonnaDestra);
+});
+
+
+
+
+function nascondiColonna() {
+  let mainRightOpened = document.getElementById('mainRightOpened');
+  mainRightOpened.style.display = 'none';
+  
+  let colCentral = document.querySelector('.col-8');
+  colCentral.classList.add('col-10');
 }
 document.addEventListener("DOMContentLoaded", function () {
   let rightColumn = document.getElementById("mainRightOpened");
